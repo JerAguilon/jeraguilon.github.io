66d7d4a53a0fa31c7b4d5a41fa6c6d963a9dcdb6
jeremy <jeraguilon@gmail.com>
2016-09-11 06:37:52 -0400

    Added progress bars

--- js/indexScript.js
+++ js/indexScript.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
              $target = $(target);
 
          $('html, body').stop().animate({
-             'scrollTop': $target.offset().top
+             'scrollTop': $target.offset().top - 80
          }, 900, 'swing', function () {
              window.location.hash = target;
          });
