550140f7afd49347c851581f7fe8d771eb92924b
Jeremy Aguilon <jeraguilon@gmail.com>
2018-05-13 17:00:40 -0600

    Lazy loading improved for articles

--- src/components/ArticleRenderer/ArticleComponents.tsx
+++ src/components/ArticleRenderer/ArticleComponents.tsx
@@ -1,16 +1,10 @@
-import { CramScore } from 'components/CramScore';
-
-// Images: TODO: refactor this into a nice factory
-const ValidationImage = require('static/img/ml/validationset.png');
-const KaggleTaxi = require('static/img/ml/kaggle_taxi.png');
-
 export default {
     ranking_interview_questions_by_cram_score: {
-        "CramScore": CramScore,
+        "CramScore": require('components/CramScore/CramScore.tsx').CramScore,
     },
     sales_prediction_a_deep_learning_approach: {
-        "ValidationImage": ValidationImage,
-        "KaggleTaxi": KaggleTaxi
+        "ValidationImage": require('static/img/ml/validationset.png'),
+        "KaggleTaxi": require('static/img/ml/kaggle_taxi.png'),
     }
 };
 
